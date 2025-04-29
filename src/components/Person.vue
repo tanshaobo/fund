a6523131ec0b683d09e8dc4091fceb75750e4559
tanshaobo <18235140339@163.com>
2025-04-29 23:14:21 +0800

    test: tset

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -8,23 +8,29 @@
 		<button @click="changeAge">修改年龄</button>
 		<p>电话：{{ tel }}</p>
 		<button @click="showTel">显示电话</button>
+		{{ sum }}
 	</div>
 </template>
 
 <script lang="ts" setup>
-import { reactive, toRefs } from 'vue'
+import { ref, reactive, toRefs } from 'vue'
 
-const obj = reactive({
+let obj = reactive({
 	name: '张三',
 	sex: '保密',
 	age: 18,
 	tel: '',
 })
+let sum = ref(0)
+console.log(sum)
 const changeName = () => {
 	obj.name = '李四'
 }
 const showSex = () => {
 	obj.sex = '武装直升机'
+	// Object.assign(obj, { sex: '沃尔玛购物袋' })
+
+	console.log('obj', obj)
 }
 const changeAge = () => {
 	obj.age += 1
@@ -32,6 +38,7 @@ const changeAge = () => {
 const showTel = () => {
 	obj.tel = '12345678901'
 }
+
 const { name, sex, age, tel } = toRefs(obj)
 </script>
 
